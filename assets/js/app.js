724bdefdfe5be09b3de9686ad3c90b1e79a3fe81
Nabin Singh <nabin@skilluplabs.com.au>
2024-03-04 20:32:03 +1100

    Added add item feature

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+(() => {
+    const allItems = [];
+
+
+    function addNewItem(event) {
+        event.preventDefault();
+
+        const inputElement = document.querySelector("input");
+        let newItem = inputElement.value;
+    
+        allItems.push(newItem);
+        populateItems(allItems);
+        inputElement.value = "";
+    }
+
+
+    function populateItems(allItems) {
+        const listsElement = document.getElementsByClassName("lists")[0];
+        
+        const allElements = [] ;
+        allItems.forEach((item) => {
+            allElements.push(` <div>
+                <span>${item}</span>
+                <input type="checkbox" >
+            </div>`);
+        });
+        listsElement.innerHTML = allElements.join("");
+    }
+    
+    document.getElementById("add_new_btn").addEventListener("click", addNewItem);
+})();
