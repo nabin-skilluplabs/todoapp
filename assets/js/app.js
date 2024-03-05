4dd1572660dd1ac34af31e8e02dc1178dd4ae54e
Nabin Singh <nabin@skilluplabs.com.au>
2024-03-05 20:47:09 +1100

    Added additional features

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 (() => {
-    const allItems = [];
+    let allItems = ["Study Web Development Coures", "Go to Moviees", "Build an application"];
 
     function populateTotalItems (count) {
         const totalElement = document.getElementById("total");
@@ -21,8 +21,19 @@
         else {
             inputElement.classList.add("error-input");
         }
+
+        toggleViewButton();
     }
 
+
+    function toggleViewButton() {
+        if(allItems.length > 5) {
+            document.getElementById("view_all").style.display = "inline";
+        }
+        else {
+            document.getElementById("view_all").style.display = "none";
+        }
+    }
 
     function populateItems(allItems) {
         const listsElement = document.getElementsByClassName("lists")[0];
@@ -31,10 +42,40 @@
         allItems.forEach((item) => {
             allElements.push(`<div>
                 <span>${item}</span>
-                <input type="checkbox" >
+                <input class="check-item" type="checkbox" >
             </div>`);
         });
         listsElement.innerHTML = allElements.join("");
+        handleCheckbox();
+    }
+
+    function handleCheckbox() {
+        const checkItems = document.querySelectorAll(".check-item");
+        checkItems.forEach((checkItem) => {
+            checkItem.addEventListener("click", (event) => {
+                const checkElement = event.target;
+                const itemValue = checkElement.parentElement.childNodes[1].textContent;
+                const itemIndex = allItems.indexOf(itemValue);
+                console.log({itemIndex});
+                allItems = allItems.filter(item => {
+                    return item !== itemValue;
+                });
+                populateItems(allItems);
+            });
+        });
+        
+    }
+
+    function toggleView(event) {
+        if(event.target.innerText === "View All") {
+            populateItems(allItems);
+            event.target.innerText = "Vew Less";
+        }
+        else {
+            populateItems(allItems.slice(0, 5));
+            event.target.innerText = "View All";
+        }
+        
     }
     
     document.querySelector("input").addEventListener("focus", () => {
@@ -42,4 +83,8 @@
         inputElement.classList.remove("error-input");
     });
     document.getElementById("add_new_btn").addEventListener("click", addNewItem);
+
+    document.querySelector("#view_all").addEventListener("click", toggleView);
+    toggleViewButton();
+    populateItems(allItems);
 })();
